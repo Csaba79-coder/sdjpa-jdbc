e06624142596ea4bb46a6bf9173a5802b7575557
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-31 20:18:20 +0100

    Implement Get Author By Id, test operates

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -3,10 +3,48 @@ package guru.springframework.jdbc.dao;
 import guru.springframework.jdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+
+    private final DataSource source;
+
+    public AuthorDaoImpl(DataSource source) {
+                this.source = source;
+    }
+
     @Override
     public Author getById(Long id) {
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = source.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM author WHERE id =" + id);
+
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name")); // here can come column name or index!
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    public DataSource getSource() {
+        return source;
     }
 }
